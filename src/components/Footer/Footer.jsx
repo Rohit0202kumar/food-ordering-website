651761230c43c98ef36db5c6c8901d07f1ff6b36
Rohit0202kumar <rohitrajbhar0202@gmail.com>
2024-09-18 23:21:26 +0530

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -35,7 +35,7 @@ const Footer = () => {
                 </div>
             </div>
             <hr />
-            <p className="footer-copyright">Copyright 2024 © Tomato.com-All Right Reserved </p>
+            <p className="footer-copyright">Created By Rohit Kumar</p>
         </div>
     </div>
   )
